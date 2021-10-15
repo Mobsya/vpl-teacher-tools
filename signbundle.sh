6705e10f548c722bd9a8967485e98d1d9a80e88d
Michael Bonani <michael.bonani@mobsya.org>
2021-10-15 09:08:15 +0200

    manualy sign with --deep

--- signbundle.sh
+++ signbundle.sh
@@ -15,7 +15,7 @@ sign() {
     if [ -z "$IDENTITY" ]; then
         echo "Identity not provided, not signing"
     else
-        codesign --verify --verbose --timestamp --entitlements allow.entitlements -s "$IDENTITY" "$@"
+        codesign --verify --verbose=4 --timestamp --entitlements allow.entitlements -s "$IDENTITY" "$@"
     fi
 }
 
@@ -25,6 +25,14 @@ add_to_group $(realpath "$DEST/Contents/Info.plist")
 chmod 644 $(realpath "$DEST/Contents/Info.plist")
 
 MAIN_DIR="$DEST/Contents/MacOS"
+
+for fw in $(ls "$DEST/Contents/Frameworks")
+do
+    echo "Signing $DEST/Contents/Frameworks/$fw"
+    sign $(realpath "$DEST/Contents/Frameworks/$fw")
+done
+
+unzip $(realpath "$MAIN_DIR/lib/library.zip") $(realpath "$MAIN_DIR/lib/library")
 
 for plugin in $(find $MAIN_DIR -name '*.dylib')
 do
@@ -38,8 +46,11 @@ do
     sign $(realpath "$so")
 done
 
+cd $(realpath "$MAIN_DIR/lib/library")
+zip -r library.zip ./*
+
 for binary in "launch_objc"
 do
     echo "Signing $MAIN_DIR/$binary"
-    sign --options=runtime $(realpath "$MAIN_DIR/$binary")
+    sign --options=runtime --deep $(realpath "$MAIN_DIR/$binary")
 done
